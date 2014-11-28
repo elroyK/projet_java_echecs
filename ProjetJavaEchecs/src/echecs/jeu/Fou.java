4c53aca28e52630744cf48261dedcd5e03fa0865
Enzo Yaranossian <eyaranossian@outlook.com>
2014-11-28 14:19:46 +0100

    test commit

--- ProjetJavaEchecs/src/echecs/jeu/Fou.java
+++ ProjetJavaEchecs/src/echecs/jeu/Fou.java
@@ -84,7 +84,7 @@ package echecs.jeu;
 			//TODO Tests savoir première rencontrée sur chaque axe méthode à part
 			//TODO Eclaircir cases jusque première pièce incluse
 			//TODO Mouvement en lui même
-			//TODO test commit Enzo		
+		
 		}
 	}
 					
