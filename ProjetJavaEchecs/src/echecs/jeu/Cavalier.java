7052f3619c7bfd3d97aa8e8e5866a786860513e5
Cyril Pierret <cyril.pierret.pierrard@gmail.com>
2014-12-02 14:20:07 +0100

    TEST COMMIT 1

--- ProjetJavaEchecs/src/echecs/jeu/Cavalier.java
+++ ProjetJavaEchecs/src/echecs/jeu/Cavalier.java
@@ -3,7 +3,7 @@ package echecs.jeu;
  * Classe Cavalier, représentant la pièce Cavalier
  * @author Leroy Christophe - Pierret Cyril - Yaranossian Enzo
  * date de création : 26/11/14
- * date de modification : 26/11/14
+ * date de modification : 02/12/14
  */
 
 public class Cavalier extends Piece {
