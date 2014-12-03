11ac62cf4a1f76f3478ce54ef7b1fbdab927d2e4
Enzo Yaranossian <eyaranossian@outlook.com>
2014-12-04 00:18:44 +0100

    Classe Tour
    Implémentation de la méthode genererPos() et move() -- javadoc écrite

--- ProjetJavaEchecs/src/be/ephec/echecs/jeu/Tour.java
+++ ProjetJavaEchecs/src/be/ephec/echecs/jeu/Tour.java
@@ -3,9 +3,88 @@ package be.ephec.echecs.jeu;
  * Classe Tour, représentant la pièce Tour
  * @author Leroy Christophe - Pierret Cyril - Yaranossian Enzo
  * date de création : 26/11/14
- * date de modification : 26/11/14
+ * date de modification : 04/12/14
  */
 
-public class Tour {
+public class Tour extends Piece {
+	final static int NBMOV = 14;
+	protected boolean estRoquable;
 
-}
+	
+	/**
+	 * Constructeur de la classe Tour
+	 * @param x : position en x
+	 * @param y : position en y
+	 * @param addImage : adresse de l'image
+	 */
+	
+	public Tour (int x, int y, String addImage) {
+		super("Tour",addImage,true,x,y);
+	}
+	
+	/**
+	 * Procédure qui génère l'ensemble des positions possible de la pièce à partir
+	 * de sa position actuelle
+	 * @return work = tableau de la classe Position, contenant un couple de coord (x,y)
+	 */
+
+	//TODO : completer l'algo (boucle générale ou pas ? ...) 
+	public Position[] genererPos(){
+		
+		Position work [] = new Position[NBMOV]; 
+		int tempX = pos.getX() + 1;
+		int tempY = pos.getY() + 1;
+		int i = 0;
+		
+		/*boucle pour l'horizontale droite */
+		while ((tempX <= 7)) {
+			work[i].setX(tempX);
+			work[i].setY(tempY);
+			i++;
+			tempX++;
+		}
+		tempX = pos.getX() - 1;
+		
+		
+		/*boucle pour l'horizontale gauche*/
+		while ((tempX >= 0)) {
+			work[i].setX(tempX);
+			work[i].setY(tempY);
+			i++;
+			tempX--;
+		}
+		tempX = pos.getX() + 1;
+		
+		
+		/*boucle pour la verticale haute*/
+		while ((tempY <= 7)) {
+			work[i].setY(tempY);
+			work[i].setX(tempX);
+			i++;
+			tempY++;
+		}
+		tempY = pos.getY() - 1;
+		
+		/*boucle pour la verticale basse*/
+		while (tempY <= 0) {
+			work[i].setX(tempX);
+			work[i].setY(tempY);
+			i++;
+			tempY--;
+			}
+		tempY = pos.getY() + 1;
+		
+		return work;
+	}
+
+	
+	public void move(Echiquier plateau) {
+		Position tbPos[] = this.genererPos();
+		this.genererCaseCliquable(plateau, tbPos);
+		// TODO : AFFICHER LES CASES DISPOBIBLES EN VERT
+		// TODO : CHOPER LA POSITION CLIQUE PAR LE JOUEUR
+		Position zone = new Position(0,0); // A IMPLEMENTER AVEC LA POSITION DU JOUEUR
+		this.deplacement(plateau,zone);
+		}
+	}
+
