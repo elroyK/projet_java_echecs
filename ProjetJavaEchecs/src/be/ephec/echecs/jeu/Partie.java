5ed3db3bfd29a86de5cc945ca80fcbf753808931
Christophe Leroy <elroy@live.be>
2014-12-09 17:58:20 +0100

    Ajout des icônes des pièces sur les cases à l'initialisation

--- ProjetJavaEchecs/src/be/ephec/echecs/jeu/Partie.java
+++ ProjetJavaEchecs/src/be/ephec/echecs/jeu/Partie.java
@@ -3,7 +3,6 @@ package be.ephec.echecs.jeu;
 import be.ephec.echecs.gui.*;
 
 import javax.swing.*;
-
 import java.awt.*;
 /**
  * Classe Partie, programme mère du projet
@@ -22,9 +21,10 @@ public class Partie {
 	
 	public static void main(String[] args) {
 		Partie game = new Partie();	
-		Echiquier.main(null);
-		game.initialisation();
-		/*	while (game.finPartie()==true) {
+		do {
+			//Echiquier.main(null);
+			game.initialisation();
+			while (game.finPartie()==true) {
 				if (game.settings.getJoueurActuel() == 1)
 					// TOUR DU JOUEUR 1
 				{
@@ -38,19 +38,20 @@ public class Partie {
 					 *  			p.s. : s'il clique sur une de ces pièces on rechange le premier clic ?
 					 *  		4.Après le second clic
 					 *  			faire le déplacement et eventuelles kills
-					 				
+					 */				
 					
 				} else
 					// TOUR DU JOUEUR 2
 				{
 					/*
 					 *  TODO :  .... inversément 
-					 
+					 */
 				}
 				game.settings.chgmJoueurActuel();
-			}*/
+			}
 			
 			// TODO : A la fin de la partie demander l'accord de deux joueurs pour recommencer une partie
+		} while (game.reset==true);
 		 
 		 
 	}
@@ -59,7 +60,7 @@ public class Partie {
 	 */
 	
 	public Partie() {
-		this.reset = true;
+		this.reset = false;
 	}
 	
 	/**
@@ -67,87 +68,109 @@ public class Partie {
 	 */
 	public void initialisation() {
 		// Initialisation pièces blanches
-		this.plateau.echiq[0][6].setIcon(new ImageIcon(getClass().getResource("/img/pionB.gif")));
+		plateau.main(null);
 		this.jA.tbPiece[0] = new Pion(0,6,"/img/pionB.gif");
 		this.plateau.echiq[0][6].setEstOccupe("BLANC");
-		this.plateau.echiq[0][6].actualise("/img/pionB.gif");
-		
-		
+		this.plateau.echiq[0][6].actualise(this.jA.tbPiece[0].getAddIcone());
 		this.jA.tbPiece[1] = new Pion(1,6,"/img/pionB.gif");
 		this.plateau.echiq[1][6].setEstOccupe("BLANC");
-		this.plateau.echiq[1][6].setIcon(new ImageIcon(getClass().getResource("/img/pionB.gif")));
-		
+		this.plateau.echiq[1][6].actualise(this.jA.tbPiece[1].getAddIcone());
 		this.jA.tbPiece[2] = new Pion(2,6,"/img/pionB.gif");
 		this.plateau.echiq[2][6].setEstOccupe("BLANC");
-		this.plateau.echiq[1][6].actualise("/img/pionB.gif");
-		
-		this.plateau.echiq[2][6].setIcon(new ImageIcon(getClass().getResource("/img/pionB.gif")));
-		
-		
-		this.plateau.setVisible(true);
-		/*
+		this.plateau.echiq[2][6].actualise(this.jA.tbPiece[2].getAddIcone());
 		this.jA.tbPiece[3] = new Pion(3,6,"/img/pionB.gif");
 		this.plateau.echiq[3][6].setEstOccupe("BLANC");
+		this.plateau.echiq[3][6].actualise(this.jA.tbPiece[3].getAddIcone());
 		this.jA.tbPiece[4] = new Pion(4,6,"/img/pionB.gif");
 		this.plateau.echiq[4][6].setEstOccupe("BLANC");
+		this.plateau.echiq[4][6].actualise(this.jA.tbPiece[4].getAddIcone());
 		this.jA.tbPiece[5] = new Pion(5,6,"/img/pionB.gif");
 		this.plateau.echiq[5][6].setEstOccupe("BLANC");
+		this.plateau.echiq[5][6].actualise(this.jA.tbPiece[5].getAddIcone());
 		this.jA.tbPiece[6] = new Pion(6,6,"/img/pionB.gif");
 		this.plateau.echiq[6][6].setEstOccupe("BLANC");
+		this.plateau.echiq[6][6].actualise(this.jA.tbPiece[6].getAddIcone());
 		this.jA.tbPiece[7] = new Pion(7,6,"/img/pionB.gif");
 		this.plateau.echiq[7][6].setEstOccupe("BLANC");
+		this.plateau.echiq[7][6].actualise(this.jA.tbPiece[7].getAddIcone());
 		
-		this.jA.tbPiece[8] = new Cavalier(1,7,"img/cavalierB.gif");
+		this.jA.tbPiece[8] = new Cavalier(1,7,"/img/cavalierB.gif");
 		this.plateau.echiq[1][7].setEstOccupe("BLANC");
-		this.jA.tbPiece[9] = new Cavalier(6,7,"img/cavalierB.gif");
+		this.plateau.echiq[1][7].actualise(this.jA.tbPiece[8].getAddIcone());
+		this.jA.tbPiece[9] = new Cavalier(6,7,"/img/cavalierB.gif");
 		this.plateau.echiq[6][7].setEstOccupe("BLANC");
+		this.plateau.echiq[6][7].actualise(this.jA.tbPiece[9].getAddIcone());
 		this.jA.tbPiece[10] = new Tour(0,7,"/img/tourB.gif");
 		this.plateau.echiq[0][7].setEstOccupe("BLANC");
+		this.plateau.echiq[0][7].actualise(this.jA.tbPiece[10].getAddIcone());
 		this.jA.tbPiece[11] = new Tour(7,7,"/img/tourB.gif");
 		this.plateau.echiq[7][7].setEstOccupe("BLANC");
+		this.plateau.echiq[7][7].actualise(this.jA.tbPiece[11].getAddIcone());
 		this.jA.tbPiece[12] = new Fou(2,7,"/img/fouB.gif");
 		this.plateau.echiq[2][7].setEstOccupe("BLANC");
+		this.plateau.echiq[2][7].actualise(this.jA.tbPiece[12].getAddIcone());
 		this.jA.tbPiece[13] = new Fou(5,7,"/img/fouB.gif");
 		this.plateau.echiq[5][7].setEstOccupe("BLANC");
+		this.plateau.echiq[5][7].actualise(this.jA.tbPiece[13].getAddIcone());
 		this.jA.tbPiece[14] = new Roi(3,7,"/img/roiB.gif");
 		this.plateau.echiq[3][7].setEstOccupe("BLANC");
+		this.plateau.echiq[3][7].actualise(this.jA.tbPiece[14].getAddIcone());
 		this.jA.tbPiece[15] = new Reine(4,7,"/img/reineB.gif");
 		this.plateau.echiq[4][7].setEstOccupe("BLANC");
+		this.plateau.echiq[4][7].actualise(this.jA.tbPiece[15].getAddIcone());
 		
 		// Initialisation pièces noires
-		this.jB.tbPiece[0] = new Pion(0,1,"img/pionN.gif");
+		this.jB.tbPiece[0] = new Pion(0,1,"/img/pionN.gif");
 		this.plateau.echiq[0][1].setEstOccupe("NOIR");
-		this.jB.tbPiece[1] = new Pion(1,1,"img/pionN.gif");
+		this.plateau.echiq[0][1].actualise(this.jB.tbPiece[0].getAddIcone());
+		this.jB.tbPiece[1] = new Pion(1,1,"/img/pionN.gif");
 		this.plateau.echiq[1][1].setEstOccupe("NOIR");
-		this.jB.tbPiece[2] = new Pion(2,1,"img/pionN.gif");
+		this.plateau.echiq[1][1].actualise(this.jB.tbPiece[1].getAddIcone());
+		this.jB.tbPiece[2] = new Pion(2,1,"/img/pionN.gif");
 		this.plateau.echiq[2][1].setEstOccupe("NOIR");
-		this.jB.tbPiece[3] = new Pion(3,1,"img/pionN.gif");
+		this.plateau.echiq[2][1].actualise(this.jB.tbPiece[2].getAddIcone());
+		this.jB.tbPiece[3] = new Pion(3,1,"/img/pionN.gif");
 		this.plateau.echiq[3][1].setEstOccupe("NOIR");
-		this.jB.tbPiece[4] = new Pion(4,1,"img/pionN.gif");
+		this.plateau.echiq[3][1].actualise(this.jB.tbPiece[3].getAddIcone());
+		this.jB.tbPiece[4] = new Pion(4,1,"/img/pionN.gif");
 		this.plateau.echiq[4][1].setEstOccupe("NOIR");
-		this.jB.tbPiece[5] = new Pion(5,1,"img/pionN.gif");
+		this.plateau.echiq[4][1].actualise(this.jB.tbPiece[4].getAddIcone());
+		this.jB.tbPiece[5] = new Pion(5,1,"/img/pionN.gif");
 		this.plateau.echiq[5][1].setEstOccupe("NOIR");
-		this.jB.tbPiece[6] = new Pion(6,1,"img/pionN.gif");
+		this.plateau.echiq[5][1].actualise(this.jB.tbPiece[5].getAddIcone());
+		this.jB.tbPiece[6] = new Pion(6,1,"/img/pionN.gif");
 		this.plateau.echiq[6][1].setEstOccupe("NOIR");
-		this.jB.tbPiece[7] = new Pion(7,1,"img/pionN.gif");
+		this.plateau.echiq[6][1].actualise(this.jB.tbPiece[6].getAddIcone());
+		this.jB.tbPiece[7] = new Pion(7,1,"/img/pionN.gif");
 		this.plateau.echiq[7][1].setEstOccupe("NOIR");
+		this.plateau.echiq[7][1].actualise(this.jB.tbPiece[7].getAddIcone());
 		
-		this.jB.tbPiece[8] = new Cavalier(1,0,"img/cavalierN.gif");
+		this.jB.tbPiece[8] = new Cavalier(1,0,"/img/cavalierN.gif");
 		this.plateau.echiq[1][0].setEstOccupe("NOIR");
-		this.jB.tbPiece[9] = new Cavalier(6,0,"img/cavalierN.gif");
+		this.plateau.echiq[1][0].actualise(this.jB.tbPiece[8].getAddIcone());
+		this.jB.tbPiece[9] = new Cavalier(6,0,"/img/cavalierN.gif");
 		this.plateau.echiq[6][0].setEstOccupe("NOIR");
-		this.jB.tbPiece[10] = new Tour(0,0,"img/tourN.gif");
+		this.plateau.echiq[6][0].actualise(this.jB.tbPiece[9].getAddIcone());
+		this.jB.tbPiece[10] = new Tour(0,0,"/img/tourN.gif");
 		this.plateau.echiq[0][0].setEstOccupe("NOIR");
-		this.jB.tbPiece[11] = new Tour(7,0,"img/tourN.gif");
+		this.plateau.echiq[0][0].actualise(this.jB.tbPiece[10].getAddIcone());
+		this.jB.tbPiece[11] = new Tour(7,0,"/img/tourN.gif");
 		this.plateau.echiq[7][0].setEstOccupe("NOIR");
-		this.jB.tbPiece[12] = new Fou(2,0,"img/fouN.gif");
+		this.plateau.echiq[7][0].actualise(this.jB.tbPiece[11].getAddIcone());
+		this.jB.tbPiece[12] = new Fou(2,0,"/img/fouN.gif");
 		this.plateau.echiq[2][0].setEstOccupe("NOIR");
-		this.jB.tbPiece[13] = new Fou(5,0,"img/fouN.gif");
+		this.plateau.echiq[2][0].actualise(this.jB.tbPiece[12].getAddIcone());
+		this.jB.tbPiece[13] = new Fou(5,0,"/img/fouN.gif");
 		this.plateau.echiq[5][0].setEstOccupe("NOIR");
-		this.jB.tbPiece[14] = new Roi(4,0,"img/roiN.gif");
+		this.plateau.echiq[5][0].actualise(this.jB.tbPiece[13].getAddIcone());
+		this.jB.tbPiece[14] = new Roi(3,0,"/img/roiN.gif");
 		this.plateau.echiq[4][0].setEstOccupe("NOIR");
-		this.jB.tbPiece[15] = new Reine(3,0,"img/reineN.gif");
-		this.plateau.echiq[5][0].setEstOccupe("NOIR");	*/	
+		this.plateau.echiq[4][0].actualise(this.jB.tbPiece[14].getAddIcone());
+		this.jB.tbPiece[15] = new Reine(4,0,"/img/reineN.gif");
+		this.plateau.echiq[3][0].setEstOccupe("NOIR");
+		this.plateau.echiq[3][0].actualise(this.jB.tbPiece[15].getAddIcone());
+		
+		this.plateau.setVisible(true);
 }
 			
 	
