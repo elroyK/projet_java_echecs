9ba3b0899e5b368d0e22580208577d38ddc7d5b9
Christophe Leroy <elroy@live.be>
2014-12-15 20:51:07 +0100

    Ajout de la fenêtre de fin de Partie Fin de partie fonctionnel (si Echec & mat, roi pris ou pat)

---
+++ ProjetJavaEchecs/src/be/ephec/echecs/gui/FenFinPartie.java
@@ -0,0 +1,53 @@
+package be.ephec.echecs.gui;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import be.ephec.echecs.jeu.Param;
+import be.ephec.echecs.jeu.Partie;
+
+public class FenFinPartie extends JFrame {
+
+	private JPanel contentPane;
+	
+	public FenFinPartie(Partie game) {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 345, 99);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		JLabel labelFin = new JLabel("");
+		
+		if (game.estEnd()){
+			String winner = "";
+			switch (Partie.getjEnCours().getCouleur()){
+			case Param.BLANC : winner = "joueur 2";break;
+			case Param.NOIR : winner = "joueur 1";break;
+			}
+		
+			labelFin = new JLabel("Le "+ winner +" remporte la partie !");
+		}
+		
+		if (game.estPat()){
+			labelFin = new JLabel("Egalité !");
+		}
+		
+		contentPane.add(labelFin, BorderLayout.NORTH);
+		
+		JButton btnOk = new JButton("Fin");
+		contentPane.add(btnOk, BorderLayout.CENTER);
+		btnOk.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				System.exit(0);
+			}
+		});
+	}
+}
