7038cfd964c53f51f730fa31821722c34ff63a7b
eyaranossian <eyaranossian@outlook.com>
2014-12-15 20:35:03 +0100

    package info dossier images

---
+++ ProjetJavaEchecs/src/img/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * Images utilisées pour représenter les pièces 
+ * 
+ * source : Freepik => http://www.freepik.com/free-vector/chessmen-pack_750287.htm
+ * 
+ */
+/**
+ * @author Leroy Christophe - Pierret Cyril - Yaranossian Enzo
+ *
+ */
+package img;
